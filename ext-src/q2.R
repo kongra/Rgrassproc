6162df0768f093f0e7f3a00ffe11287eba3f149c
Konrad Grzanek <kongra@gmail.com>
2015-10-19 18:14:45 +0200

    Added parallel processing with snow cluster. Tested with SOCK type cluster. MPI sucks by now.

--- ext-src/q2.R
+++ ext-src/q2.R
@@ -26,38 +26,38 @@ x <- normalizeTo1(readData())
 system.time(ll <- rCr(x, k = 100, from = -12, to = 0))
 ## OK. 25.5 s na moim 1 rdzeniu AMD E-450
 
-## ggplot(ll, aes(LogY, LogFY)) + geom_point()
+## ggplot(ll, aes(log.r, log.Cr)) + geom_point()
 ## ggsave("Fig1.pdf")
-#
-# ## WYZNACZAMY WSPÓŁCZYNNIK NACHYLENIA NA CZĘŚCI LINIOWEJ, TUTAJ TO
-# ## JEST PRZEDZIAŁ [-10, -2.5]
-# llin <- ll[LogY >= -10.0 & LogY <= -2.5]
-# ## ggplot(llin, aes(LogY, LogFY)) + geom_point() + stat_smooth(method = lm)
-# ## ggsave("Fig2.pdf")
-#
-# cor(llin$LogY, llin$LogFY) ## 0.9998858 - B. ZNACZNA KORELACJA, MAMY LINIĘ
-# fit <- lm(LogFY ~ LogY, data = llin)
-#
-# ## STATYSTYCZNIE ZNACZĄCE NACHYLENIE WYNOSI 0.958:
-# summary(fit)
-# ## Call:
-# ## lm(formula = LogFY ~ LogY, data = llin)
-#
-# ## Residuals:
-# ##       Min        1Q    Median        3Q       Max
-# ## -0.052254 -0.026775 -0.000752  0.030878  0.043374
-#
-# ## Coefficients:
-# ##             Estimate Std. Error t value Pr(>|t|)
-# ## (Intercept)  1.39153    0.01235   112.6   <2e-16 ***
-# ## LogY         0.95801    0.00187   512.4   <2e-16 ***
-# ## ---
-# ## Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
-#
-# ## Residual standard error: 0.03193 on 60 degrees of freedom
-# ## Multiple R-squared:  0.9998,	Adjusted R-squared:  0.9998
-# ## F-statistic: 2.626e+05 on 1 and 60 DF,  p-value: < 2.2e-16
-#
+
+## WYZNACZAMY WSPÓŁCZYNNIK NACHYLENIA NA CZĘŚCI LINIOWEJ, TUTAJ TO
+## JEST PRZEDZIAŁ [-10, -2.5]
+llin <- ll[log.r >= -10.0 & log.r <= -2.5]
+## ggplot(llin, aes(log.r, log.Cr)) + geom_point() + stat_smooth(method = lm)
+## ggsave("Fig2.pdf")
+
+cor(llin$log.r, llin$log.Cr) ## 0.9998858 - B. ZNACZNA KORELACJA, MAMY LINIĘ
+fit <- lm(log.Cr ~ log.r, data = llin)
+
+## STATYSTYCZNIE ZNACZĄCE NACHYLENIE WYNOSI 0.958:
+summary(fit)
+## Call:
+## lm(formula = log.Cr ~ log.r, data = llin)
+
+## Residuals:
+##       Min        1Q    Median        3Q       Max
+## -0.052254 -0.026775 -0.000752  0.030878  0.043374
+
+## Coefficients:
+##             Estimate Std. Error t value Pr(>|t|)
+## (Intercept)  1.39153    0.01235   112.6   <2e-16 ***
+## log.r        0.95801    0.00187   512.4   <2e-16 ***
+## ---
+## Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+## Residual standard error: 0.03193 on 60 degrees of freedom
+## Multiple R-squared:  0.9998,	Adjusted R-squared:  0.9998
+## F-statistic: 2.626e+05 on 1 and 60 DF,  p-value: < 2.2e-16
+
 # ## WERSJA ZRÓWNOLEGLONA.
 # ## library(snow)
 #
---
+++ ext-src/q2par.R
@@ -0,0 +1,71 @@
+## Copyright (c) Konrad Grzanek
+## Created 2015-10-19
+
+library(data.table)
+library(ggplot2)
+library(Rgrassproc)
+library(snow)
+
+DATAFILE <- "input.csv"
+
+readData <- function() {
+  dt <- fread(DATAFILE)
+  x  <- dt[[1]]
+  as.numeric(gsub(",", ".", x))
+}
+
+normalizeTo1 <- function(x) {
+  max.x <- max(x)
+  if (max.x > 1) x <- x / max.x
+  x
+}
+
+## WCZYTANIE DANYCH
+x <- normalizeTo1(readData())
+
+## WERSJA ZRÓWNOLEGLONA.
+rCrPar <- function(cl, clustSize, x, k = 100, from = -4, to = 0) {
+  logr <- seq(from = from, to = to, length.out = k)
+  r    <- exp(logr)
+
+  ## Przygotowanie zadań
+  tsize <- as.integer(k / clustSize) # Rozmiar zakresu indeksów dla zadania
+  tasks <- list()
+  for (i in 1:clustSize) {
+    i1 <- (i - 1) * tsize + 1 # początek zakresu indeksów dla zadania
+    i2 <- i1 + tsize - 1      # koniec   zakresu indeksów dla zadania
+    tasks[[i]] <- c(i1, i2)
+  }
+
+  taskFn <- function(t, x, r, tsize) {
+    tryCatch({
+      i1   <- t[1]
+      i2   <- t[2]
+
+      Crs <- numeric(tsize)
+      for (i in seq_along(i1:i2)) Crs[i] <- corrSum(x, r[i + i1 - 1])
+      Crs
+    },
+    error = function(e) {
+      print(e)
+      stop(e)
+    })
+  }
+
+  Crs.list  <- clusterApply(cl, tasks, taskFn, x, r, tsize)
+  Cr.result <- numeric()
+  for (Crs in Crs.list) Cr.result <- c(Cr.result, Crs)
+  data.table(log.r = logr, log.Cr = log(Cr.result))
+}
+
+## CLUST_SIZE <- 2
+## cl <- makeMPIcluster(CLUST_SIZE)
+
+cl <- makeCluster(c("localhost", "localhost"), type = "SOCK")
+clusterEvalQ(cl,{
+  library(Rgrassproc)
+})
+
+system.time(ll.par <- rCrPar(cl, 2, x, from = -12, to = 0))
+
+stopCluster(cl)
